50cbbee3a7618e9411a9934af43fdb6588024456
deepakagashe18 <agashedeepak55@gmail.com>
2024-02-14 13:03:10 +0530

    auth jwt done

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,27 +1,33 @@
 const User = require('../models/user.model')
 
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
 
 const signup = async (req,res) => {
-    const {student_name,branch,gender,college_email,passout_batch,uid,password,degree,avg_cgpa,ssc_marks} = req.body;
+    const { linkedln_link, resume_url, isAdmin,password,location,student} = req.body;
     
     try {
-        if (!uid || !password) {
+        if (!student || !password || !location) {
             return res.status(400).json({ error: 'All fields are required!' });
         }
 
-        const oldUser = await User.findOne({ uid });
+        const oldUser = await User.findOne({ 'student.uid': student.uid });
         if(oldUser)
         {
             return res.status(409).json({msg: "User already exists!"})
         }
         
 
-        const newUser = await User.create({   
-            email,
-            password,
-        })
+
+    const newUser = await User.create({
+        student: student,
+        location: location,
+        linkedln_link: linkedln_link,
+        resume_url: resume_url,
+        password: password,
+        isAdmin: isAdmin,
+     });
+
 
         const token = await newUser.generateAuthToken();
         
@@ -32,35 +38,39 @@ const signup = async (req,res) => {
         return res.status(500).json({
             err: error, msg:"Internal server error!"
         })
+        
     }
 }
 
 
-const login =  async (req,res) => {
-    const {email,password} = req.body;
+const login = async (req, res) => {
+    const { password, student: { uid } } = req.body; // Destructure `uid` from `student` in `req.body`
+    
     try {
-        if (!email || !password) {
-            return res.status(400).json({ error: 'Provide either email or phone' });
-          }
+        if (!uid || !password) {
+            return res.status(400).json({ error: 'Both uid and password are required' });
+        }
        
-      const oldUser = await User.findOne({email})
-      if(!oldUser)
-      {
-            return res.status(404).json({msg: "User does not exists!"})
-      }
-    
-      const correctPassword = await bcrypt.compare(password,oldUser.password)
-      if(!correctPassword)
-      {
-        return res.status(400).json({msg: "Invalid Credentials!"})
-      }
+        const oldUser = await User.findOne({ 'student.uid': uid }); // Find user by uid
+        
+        if (!oldUser) {
+            return res.status(404).json({ msg: "User does not exist!" });
+        }
+        
+        const correctPassword = await bcrypt.compare(password, oldUser.password);
+        
+        if (!correctPassword) {
+            return res.status(400).json({ msg: "Invalid Credentials!" });
+        }
 
-      const token = await newUser.generateAuthToken();
+        const token = await oldUser.generateAuthToken();
     
-      res.status(200).json({result: oldUser, token});
-    } catch (error){
-        return res.status(500).json({err: error, msg:"Internal server error!"})
+        res.status(200).json({ result: oldUser, token });
+    } catch (error) {
+        return res.status(500).json({ err: error, msg: "Internal server error!" });
     }
-}
+};
+
+
 
 module.exports = {signup,login};
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -5,7 +5,7 @@ const auth = require('../middlewares/auth.middleware')
 
 const {signup,login} = require('../controllers/user.controller')
 
-router.post('/signup',auth,signup);
+router.post('/signup',signup);
 router.post('/login',auth,login);
 
 module.exports = router;
