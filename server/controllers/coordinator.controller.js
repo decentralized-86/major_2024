417cc67fd421dbc976bc09b38f3ee85ffc3b9b45
Me-cha <127575583+Me-cha@users.noreply.github.com>
2024-02-19 18:27:39 +0530

    Squashed commit of the following:
    commit 14beac4fb0f1209ea1419f2322d9ed83616bd629
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Sun Feb 18 19:52:14 2024 +0530
    
        schema
    
    commit 1af5b2e8a0da0538da95d4c2e1da096362b79d3c
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Sun Feb 18 16:49:20 2024 +0530
    
        routes changed
    
    commit d5cc9c6316be01f70b9a675b70df287670564ed8
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Fri Feb 16 12:32:09 2024 +0530
    
        student crud
    
    commit c2bb5654dc05976aaf4ec6d4b1da6fda771e3648
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Fri Feb 16 10:05:11 2024 +0530
    
        coordinator
    
    commit 50cbbee3a7618e9411a9934af43fdb6588024456
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Wed Feb 14 13:03:10 2024 +0530
    
        auth jwt done
    
    commit 7e9b11302407f0bf76d828345d74216e1807bd00
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Tue Feb 13 19:53:53 2024 +0530
    
        login backend
    
    commit a0bcbcbaafde1da6067b2d8f556f3ed105fadb55
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Mon Feb 12 19:50:47 2024 +0530
    
        .env pushed
    
    commit 7402dc3a02965ef3bc9a8a6e3f5ad1b32a234b2a
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Mon Feb 12 15:18:26 2024 +0530
    
        db connected
    
    commit f314c36baadf5b41203d9d06b53df7537d6f10bc
    Author: deepakagashe18 <agashedeepak55@gmail.com>
    Date:   Mon Feb 12 13:46:17 2024 +0530
    
        server intiated

---
+++ server/controllers/coordinator.controller.js
@@ -0,0 +1,110 @@
+const Coordinator = require('../models/coordinator.model')
+const User = require('../models/user.model')
+
+const bcrypt = require('bcrypt')
+
+const co_signup = async (req,res) => {
+    const {email,password,name,uid,branch,contact} = req.body; 
+try {
+    if(!email || !password || !name || !uid || !branch || !contact)
+    {
+        return res.status.json({err: 'All fields are required!'})
+    }
+    if (contact.length !== 10 || !/^\d+$/.test(contact)) {
+        return res.status(400).json({ error: 'Contact number must be a 10-digit number' });
+    }
+   const isAdmin = email === process.env.ADMIN_EMAIL;
+
+   const oldCoordinator = await Coordinator.findOne({email: email})
+   if(oldCoordinator)
+   {
+        return res.status(400).json({msg: 'Coordinator already exists!'});
+   }
+
+   const newCoordinator =  await Coordinator.create({
+    name,
+    email,
+    uid,
+    password,
+    branch,
+    contact,
+    isAdmin,
+   })
+
+
+   res.status(200).json({result: newCoordinator})
+
+    
+} catch (error) {
+    return res.status(500).json({msg: 'Internal server error!'});
+}
+}
+
+//Coordinator login is in user.controller.js
+
+//Students CRUD: 
+const getStudentDetails = async (req,res) => {
+    try {
+        const studentList = await User.find({});
+        res.status(200).json({studentList})
+        
+    } catch (error) {
+        return res.status(500).json({err: error,msg: 'Internal server error!'})
+    }
+}
+
+const getStudent = async(req,res) => {
+    const id = req.params.id;
+    try {
+        const student = await User.findById(id);
+        if(!student)
+        {
+            return res.status(404).json({msg: `Student does not exists with id:${id}!`});
+        }
+        res.status(200).json({student})
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({err: error,msg: 'Internal server error!'})
+    }
+}
+
+
+
+    const updateStudent = async (req, res) => {
+        const id = req.params.id;
+      
+        try {
+          const updatedStudent = await User.findByIdAndUpdate(id,req.body,{
+            new: true,
+            runValidators: true,
+          })
+      
+          if (!updatedStudent) {
+            return res.status(404).json({ msg: `Student does not exist with id: ${id}` });
+          }
+      
+          res.status(200).json({ student: updatedStudent });
+        } catch (error) {
+          console.error('Error:', error);
+          res.status(500).json({ err: error, msg: 'Internal server error!' });
+        }
+      };
+      
+const deleteStudent = async(req,res) => {
+    const id = req.params.id;
+    try {
+        const student = await User.findByIdAndDelete(id);
+        if(!student)
+        {
+            return res.status(404).json({msg: "Student does not exists!"});
+        }
+        res.status(200).json({student, msg: `Student deleted successfully with id:${id}`})
+    } catch (error) {
+        return res.status(500).json({err: error,msg: 'Internal server error!'})
+    }
+}
+
+
+
+module.exports = {co_signup,getStudentDetails,getStudent,updateStudent,deleteStudent};
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app  = express();
+const cors = require('cors')
+
+require('dotenv').config();
+
+const connect = require('./db/connect')
+const auth = require('./routes/auth.route')
+const coordinator = require('./routes/coordinator.route')
+
+app.use(cors());
+app.use(express.json()); 
+
+app.use('/api/user', auth);  //user
+app.use('/api/coordinator',coordinator);
+
+const Port = process.env.PORT ||  8080;
+
+const start = async() => {
+    try {
+        await connect(process.env.MONGO_URI);
+        app.listen(Port,()=>{
+            console.log(`Server is listening on ${Port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
