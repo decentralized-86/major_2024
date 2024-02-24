b3bfbf7184be50ecb8d31b4289bb97dfda750a64
deepakagashe18 <agashedeepak55@gmail.com>
2024-02-24 18:25:52 +0530

    commit done

--- server/controllers/jobPost.controller.js
+++ server/controllers/jobPost.controller.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const jobPost = require('../models/jobPost.model');
 const { sendNotificationEmailToAllUsers } = require('../services/mail.service');
 
@@ -30,12 +31,12 @@ const allJobPosts = async (req,res) => {
 
 
 const oneJobPost = async (req,res) => {
-    const id = req.params.id;
+    const company_id = req.params.id;
     try {
-        const job  = await jobPost.findById(id);
+        const job  = await jobPost.findById(company_id);
         if(!job)
         {
-            return res.status(404).json({message: `No Job Post with id ${id}`})
+            return res.status(404).json({message: `No Job Post with id ${company_id}`})
         }
 
         res.status(200).json({result: job});
@@ -45,12 +46,14 @@ const oneJobPost = async (req,res) => {
 }
 
 const updateJobPost = async (req,res) => {
-    const id = req.params.id;
+    const company_id = req.params.id;
     try {
-        const updateJob = await jobPost.findByIdAndUpdate(id);
+        const updateJob = await jobPost.findByIdAndUpdate(company_id,req.body,{
+            new:true,
+        });
         if(!updateJob)
         {
-            return res.status(404).json({message: `No Job Post with id ${id}` })
+            return res.status(404).json({message: `No Job Post with id ${company_id}` })
         }
 
         res.status(200).json({result: updateJob});
@@ -60,12 +63,12 @@ const updateJobPost = async (req,res) => {
 }
 
 const deleteJobPost = async (req,res) => {
-    const id = req.params.id;
+    const company_id = req.params.id;
     try {
-        const deletejob = await jobPost.findByIdAndDelete(id);
+        const deletejob = await jobPost.findByIdAndDelete(company_id);
         if(!deleteJobPost)
         {
-            return res.status(404).json({message: `No Job Post with id ${id}` })
+            return res.status(404).json({message: `No Job Post with id ${company_id}` })
         }
 
         res.status(200).json({deletejob});
@@ -75,5 +78,50 @@ const deleteJobPost = async (req,res) => {
 }
 
 
+const allAppliedStudents = async (req,res) => {
+    const company_id = req.params.id;
+    try {
+        const result = await jobPost.aggregate([
+            {
+                $match: {
+                    _id: company_id
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "candidates.uid",
+                    foreignField: "uid",
+                    as: "registered_candidates"
 
-module.exports = {addJobPost, allJobPosts, oneJobPost,updateJobPost,deleteJobPost};
+                }
+            },
+            {
+                $project: {
+                    "company_name": 1,
+                    "registered_candidates.name": 1,
+                    "registered_candidates.uid":1,
+                    "registered_candidates.college_email": 1,
+                    "registered_candidates.batch": 1,
+                    "registered_candidates.branch":1,
+                    "registered_candidates.contact": 1,
+                    "registered_candidates.department": 1,
+                    "registered_candidates.resume_url": 1,
+                }
+            }
+        ])
+
+        if(result.length === 0)
+        {
+            return res.status(404).json({sucess: true, message: `No Job Post found with id ${company_id}`})
+        }
+
+        res.status(200).json({sucess: true, companies: result})
+    } catch (error) {
+        console.log(error)
+         return res.status(500).json({err: error, message: "Internal server error!"})
+    }
+}
+
+
+module.exports = {addJobPost, allJobPosts, oneJobPost,updateJobPost,deleteJobPost,allAppliedStudents};
