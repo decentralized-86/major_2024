8c490432b25bf28778ddb18544146624161c941b
deepakagashe18 <agashedeepak55@gmail.com>
2024-03-10 18:11:17 +0530

    add coordinator

--- server/services/mail.service.js
+++ server/services/mail.service.js
@@ -6,22 +6,20 @@ const Coordinator = require("../models/coordinator.model");
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.SENDER_EMAIL, // replace with your email
-    pass: process.env.SENDER_PASS, // replace with your email password
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_PASS,
   },
 });
-
-
 
 // Function to send notification email
 const sendNotificationEmailToAllUsers = async (jobPost) => {
   try {
-    const users = await User.find(); // Assuming you have a User model
+    const users = await User.find();
     const userEmails = users.map((user) => user.college_email);
 
     const mailOptions = {
-      from: process.env.SENDER_EMAIL, // replace with your email
-      to: userEmails.join(", "), // Comma-separated list of all user emails
+      from: process.env.SENDER_EMAIL,
+      to: userEmails.join(", "),
       subject: "New Job Post Added",
       text: `A new job post "${jobPost.job_info.job_profile}" has been added. Check it out!`,
     };
@@ -30,7 +28,9 @@ const sendNotificationEmailToAllUsers = async (jobPost) => {
     console.log("Email sent:", info.response);
   } catch (error) {
     console.error("Error sending email:", error);
-    return res.status(500).json({err: error, message: "Email service error!"})
+    return res
+      .status(500)
+      .json({ err: error, message: "Email service error!" });
   }
 };
 
@@ -50,29 +50,38 @@ const trainingNotificationToAllUsers = async (training) => {
     console.log("Email sent:", info.response);
   } catch (error) {
     console.error("Error sending email:", error);
-    return res.status(500).json({err: error, message: "Email service error!"})
+    return res
+      .status(500)
+      .json({ err: error, message: "Email service error!" });
   }
-}
+};
 
-const addCoordinatorMail = async() => {
+const addCoordinatorMail = async () => {
   try {
     const coordinators = await Coordinator.find();
-    const coordinatorEmails = coordinators.map((coordinator)=> coordinator.email);
+    const coordinatorEmails = coordinators.map(
+      (coordinator) => coordinator.email
+    );
 
     const mailOptions = {
       from: process.env.SENDER_EMAIL, // replace with your email
-      to: userEmails.join(", "), // Comma-separated list of all user emails
-      subject: "New Job Post Added",
-      text: `Training session on "${training.title}". Check it out!`,
+      to: coordinatorEmails.join(", "), // Comma-separated list of all user emails
+      subject: "Invitation to become a Coordinator on CPMS",
+      text: `Sign Up  as coordinator using the below link`,
     };
 
     const info = await transporter.sendMail(mailOptions);
     console.log("Email sent:", info.response);
   } catch (error) {
     console.error("Error sending email:", error);
-    return res.status(500).json({err: error, message: "Email service error!"})
+    return res
+      .status(500)
+      .json({ err: error, message: "Email service error!" });
   }
-}
+};
 
-
-module.exports = { sendNotificationEmailToAllUsers,trainingNotificationToAllUsers,addCoordinatorMail};
+module.exports = {
+  sendNotificationEmailToAllUsers,
+  trainingNotificationToAllUsers,
+  addCoordinatorMail,
+};
