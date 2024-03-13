1208243177ac909aa873c597c4077be99eddbc0e
deepakagashe18 <agashedeepak55@gmail.com>
2024-03-13 18:43:49 +0530

    withdraw

--- server/controllers/apply.controller.js
+++ server/controllers/apply.controller.js
@@ -2,10 +2,9 @@ const Jobpost = require("../models/jobPost.model");
 
 const oneClickApply = async (req, res) => {
   try {
-    const { uid,company_id } = req.body;
-    console.log("Job Post ID:", company_id);
+    const { uid,job_id } = req.body;
 
-    const jobPost = await Jobpost.findOne({ _id: company_id });
+    const jobPost = await Jobpost.findOne({ _id: job_id });
 
     if (!jobPost) {
       return res.status(404).json({ error: "Job post not found" });
@@ -35,4 +34,26 @@ const oneClickApply = async (req, res) => {
   }
 };
 
-module.exports = { oneClickApply };
+const withdrawApply = async (req,res) => {
+  const {job_id,uid} = req.body;
+  try {
+    const jobPost = await Jobpost.findOne({_id: job_id });
+
+    if (!jobPost) {
+      return res.status(404).json({ error: "Job post not found" });
+    }
+
+    jobPost.candidates.splice(jobPost.candidates.map(candidate => candidate.uid).indexOf(uid),1);
+
+    const data = await jobPost.save();
+    res.status(200).json({success: true, message: "Application withdrawn."})
+    
+
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ err: error, message: "Internal server error!" });
+  }
+
+}
+module.exports = { oneClickApply, withdrawApply };
--- server/routes/apply.route.js
+++ server/routes/apply.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth.middleware");
-const { oneClickApply } = require("../controllers/apply.controller");
+const { oneClickApply,withdrawApply } = require("../controllers/apply.controller");
 
 router.post("/job/apply", auth, oneClickApply);
+router.post("/job/withdraw",auth,withdrawApply);
+
 
 module.exports = router;
